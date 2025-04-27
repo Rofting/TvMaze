0443a4de1b5157c7bd9952e3dd0ebb853cc7a41d
Jose <a27941@svalero.com>
2025-04-27 23:56:41 +0200

    Creamos el filtro por año

--- src/main/java/org/svalero/tvmaze/model/Image.java
+++
@@ -1,9 +0,0 @@
-package org.svalero.tvmaze.model;
-
-public class Image {
-    private String medium;
-    private String original;
-
-    public String getMedium() { return medium; }
-    public String getOriginal() { return original; }
-}
